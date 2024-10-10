00234fd9b7e1d2a550659963870dda79cabca25f
gurveenkaur574 <137752144+gurveenkaur574@users.noreply.github.com>
2024-10-10 16:39:35 +1100

    Update database.py

--- project_part3/book_catalog/app/database.py
+++ project_part3/book_catalog/app/database.py
@@ -3,7 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+# Add sslmode=require to the connection URL
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@"
+    "dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2"
+    "?sslmode=require"  # Enable SSL
+)  # os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
